aeff6fafc38e23ec56ab9715cac639d65ec54ac6
Kelvin Baiden <denlinato@gmail.com>
2021-12-31 00:38:09 +0000

    correcting few errors

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express =  require("express");
 const app  = express();
 const socketio= require("socket.io");
-const port = process.env.Port || 3000;
-const page = "dsffdf";
+
+
 
 app.use(express.static(__dirname+"/public"));
 
-const expressServer =  app.listen(port , ()=>{
+const expressServer =  app.listen(process.env.PORT || 5000, ()=>{
     // console.log("http://localhost:8");
 });
 
